7a64300c33281928d948f8f7ac6dee19f3ff5c81
Ken Beck <beckkenneth@gmail.com>
2017-04-27 17:22:26 -0500

    Reverts some changes to contract import

--- thing/tasks/contracts.py
+++ thing/tasks/contracts.py
@@ -79,8 +79,8 @@ class Contracts(APITask):
         for row in self.root.findall('result/rowset/row'):
             if self.apikey.key_type == APIKey.CORPORATION_TYPE:
                 # corp keys don't care about non-corp orders
-                #if row.attrib['forCorp'] == '0':
-                #    continue
+                if row.attrib['forCorp'] == '0':
+                    continue
                 # corp keys don't care about orders they didn't issue - another fun
                 # bug where corp keys see alliance contracts they didn't make  :ccp:
                 if self.apikey.corporation.id not in (
@@ -90,8 +90,8 @@ class Contracts(APITask):
                     continue
 
             # non-corp keys don't care about corp orders
-            #if self.apikey.key_type != APIKey.CORPORATION_TYPE and row.attrib['forCorp'] == '1':
-            #    continue
+            if self.apikey.key_type != APIKey.CORPORATION_TYPE and row.attrib['forCorp'] == '1':
+                continue
 
             contract_ids.add(int(row.attrib['contractID']))
 
