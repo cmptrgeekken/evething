bb6dca2a42a1e913a521441648285e01d380411f
Ken <beckkenneth@gmail.com>
2017-08-21 20:08:52 +0000

    Fixes price updater to massively improve performance

--- thing/tasks/priceupdater.py
+++ thing/tasks/priceupdater.py
@@ -41,7 +41,7 @@ class PriceUpdater(APITask):
 
     def run(self, api_url, taskstate_id, apikey_id, station_or_region_id):
         if self.init(taskstate_id) is False:
-            return
+            return True
 
         page_number = 1
 
@@ -52,8 +52,8 @@ class PriceUpdater(APITask):
             stations = stations_query.filter(system__constellation__region_id=station_or_region_id, is_citadel=False)
 
         if len(stations) == 0:
-            self.log_warn('No stations found for task: %d!', station_or_region_id)
-            return
+            self.log_error('No stations found for task: %d!', station_or_region_id)
+            return True
 
         primary_station = stations[0]
 
@@ -61,8 +61,8 @@ class PriceUpdater(APITask):
 
         if primary_station is None or primary_station.market_profile is None \
                 or primary_station.market_profile.sso_refresh_token is None:
-            self.log_warn('No refresh token found for station %d!' % primary_station.id)
-            return
+            self.log_error('No refresh token found for station %d!' % primary_station.id)
+            return True
 
         access_token = None
         token_expires = None
@@ -74,20 +74,28 @@ class PriceUpdater(APITask):
                 access_token, token_expires = self.get_access_token(primary_station.market_profile.sso_refresh_token)
 
             # Retrieve market data and parse the JSON
+            self.log_debug('Retrieving page %d for %d' % (page_number, station_or_region_id))
             url = api_url + str(page_number)
             data = self.fetch_esi_url(url, access_token)
-            if data is False:
-                continue
+            self.log_debug('Page %d retrieved!' % page_number)
 
+            if data is False:
+                self.log_error('API returned an error for url %s' % url)
+                break
+
+            self.log_debug('Parsing JSON for page %d' % page_number)
             try:
                 orders = json.loads(data)
             except:
                 break
+            self.log_debug('JSON parsed for page %d' % page_number)
 
             if len(orders) == 0:
                 break
 
-            station_orders = []
+            sql_inserts = []
+
+            self.log_debug('Generating SQL for page %d...' % page_number)
             for order in orders:
                 # Ignore stations we're not tracking
                 if int(order['location_id']) not in station_lookup:
@@ -98,52 +106,43 @@ class PriceUpdater(APITask):
                 price = Decimal(order['price'])
                 issued = self.parse_api_date(order['issued'], True)
 
-                station_order = StationOrder(
-                    order_id=int(order['order_id']),
-                    item_id=int(order['type_id']),
-                    station_id=int(order['location_id']),
-                    price=price,
-                    buy_order=order['is_buy_order'],
-                    volume_entered=int(order['volume_total']),
-                    volume_remaining=remaining,
-                    minimum_volume=int(order['min_volume']),
-                    issued=issued,
-                    expires=issued + timedelta(int(order['duration'])),
-                    range=order['range'],
-                    times_updated=1,
-                    last_updated=start_time,
-                )
+                order_id=int(order['order_id'])
+                item_id=int(order['type_id'])
+                station_id=int(order['location_id'])
+                buy_order=order['is_buy_order']
+                volume_entered=int(order['volume_total'])
+                volume_remaining=remaining
+                minimum_volume=int(order['min_volume'])
+                expires=issued + timedelta(int(order['duration']))
+                range=order['range']
+                times_updated=1
+                last_updated=start_time
+                
+                new_sql = "(%d, %d, %d, '%d', '%d', '%d', %0.2f, '%d', '%s', '%s', '%s', '%s', %d)" \
+                       % (order_id,
+                          item_id,
+                          station_id,
+                          volume_entered,
+                          volume_remaining,
+                          minimum_volume,
+                          price,
+                          buy_order,
+                          issued,
+                          expires,
+                          range,
+                          last_updated,
+                          times_updated)
 
-                station_orders.append(station_order)
+                sql_inserts.append(new_sql)
 
-            sql = ""
-            for o in station_orders:
-                new_sql = "('%s', '%s', '%s', '%s', '%s', '%s', %0.2f, '%d', '%s', '%s', '%s', '%s', '%s'), " \
-                       % (str(o.order_id),
-                          str(o.item_id),
-                          str(o.station_id),
-                          str(o.volume_entered),
-                          str(o.volume_remaining),
-                          str(o.minimum_volume),
-                          o.price,
-                          o.buy_order,
-                          str(o.issued),
-                          o.expires,
-                          o.range,
-                          o.last_updated,
-                          str(o.times_updated))
 
-                if len(''.join([sql, new_sql])) > 16777216:
-                    self.execute_query(sql)
-                    sql = new_sql
-                else:
-                    sql += new_sql
-
-            sql = sql.rstrip(', ')
-
-            self.execute_query(sql)
+            self.log_debug('SQL generated for page %d.' % page_number)
 
             page_number += 1
+
+            self.execute_query(sql_inserts)
+
+        self.log_debug('Orders imported for %d' % station_or_region_id)
 
         # Delete non-existent orders:
         StationOrder.objects.filter(station_id__in=list(station_lookup)).exclude(
@@ -153,8 +152,13 @@ class PriceUpdater(APITask):
         return True
 
     @transaction.atomic
-    def execute_query(self, sql):
+    def execute_query(self, sql_inserts):
         cursor = self.get_cursor()
+
+        order_ct = len(sql_inserts)
+
+        sql = ','.join(sql_inserts)
+        self.log_debug('Writing %d orders to DB' % order_ct)
 
         cursor.execute('SET autocommit=0')
         cursor.execute('SET unique_checks=0')
@@ -164,4 +168,6 @@ class PriceUpdater(APITask):
         cursor.execute('SET unique_checks=1')
         cursor.execute('SET autocommit=1')
         transaction.set_dirty()
+        
+        self.log_debug('%d orders written!' % order_ct)
 
