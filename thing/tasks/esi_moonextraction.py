8f2ea7a27ba22c5b9f96dd993255273728351d87
Ken Beck <beckkenneth@gmail.com>
2017-12-19 11:02:50 -0600

    Fixes structure service check for moon extraction

--- thing/tasks/esi_moonextraction.py
+++ thing/tasks/esi_moonextraction.py
@@ -130,7 +130,7 @@ class EsiMoonExtraction(APITask):
 
                     db_struct.save()
 
-                    StructureService.objects.filter(structure_id=db_struct.id).delete()
+                    StructureService.objects.filter(structure_id=db_struct.structure_id).delete()
                     if 'services' in struct:
                         for service in struct['services']:
                             db_service = StructureService(
